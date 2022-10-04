6cc0f32d218bfc0c12509c34142dffcea23615d8
LMedina96 <lucasgmedina96@gmail.com>
2022-10-04 00:24:24 -0300

    Working in color tags

--- src/App.vue
+++ src/App.vue
@@ -1,206 +1,336 @@
 
 <template>
   <div id="body">
-    
     <main>
       <h1>toDo List</h1>
-      
 
-      <input type="text" v-model="toDoItem" v-on:keyup.enter="addToDo" placeholder="Add a pending topic">
+      <input
+        type="text"
+        v-model="toDoItem"
+        v-on:keyup.enter="addToDo"
+        placeholder="Add a pending topic"
+      />
       <button @click="addToDo">ToDo</button>
 
       <div id="list">
         <div id="pendingList">
           <h2>Pending list</h2>
           <ul>
-            <li v-for="item in list" key="item.id">
-              <span @click="markItem(item, 1)">
-                <img src="../src/assets/square.svg" />
-                {{item}}
-              </span>
-              <i @click="deleteItem(item, 1)">✘</i>
-            </li>
+            <div class="listItem">
+              <li v-for="item in list" key="item.id">
+                <div class="itemText">
+                  <span @click="markItem(item, 1)">
+                    <img src="../src/assets/square.svg" />
+                    {{ item }}
+                  </span>
+                </div>
+                <div class="itemIcons">
+                  <img
+                    src="../src/assets/grid-gap.svg"
+                    alt="icon-for-choose-colors"
+                    class="iconColors"
+                    @click="openColorPalette"
+                  />
+                  <i @click="deleteItem(item, 1)">✘</i>
+                </div>
+              </li>
+            </div>
           </ul>
         </div>
-
 
         <div id="doneList">
           <div>
             <h2>Done list</h2>
             <ul>
-              <li v-for="itemDone in doneList" key="itemDone.id" >
-                <span @click="markItem(itemDone, 2)">
-                  <img src="../src/assets/check-square.svg" />
-                  {{itemDone}}
-                </span>
-                <i @click="deleteItem(itemDone, 2)">✘</i>
-              </li>
+              <div class="listItem">
+                <li v-for="itemDone in doneList" key="itemDone.id">
+                  <span @click="markItem(itemDone, 2)">
+                    <img
+                      src="../src/assets/check-square.svg"
+                      alt="checkBox-Unchecked"
+                    />
+                    {{ itemDone }}
+                  </span>
+                  <div class="itemIcons">
+                    <img
+                      src="../src/assets/grid-gap.svg"
+                      alt="icon-for-choose-colors"
+                      class="iconColors"
+                      @click="openColorPalette"
+                    />
+                    <i @click="deleteItem(itemDone, 2)">✘</i>
+                  </div>
+                  
+                </li>
+              </div>
             </ul>
           </div>
-
+          
           <p class="tip">Click an item in Peding List to mark as done</p>
         </div>
       </div>
-
+      
       <button @click="cleanToDoList">Clean list</button>
     </main>
+
+    <div class="colorPalette" v-if="isVisible">
+      <i @click="closeColorPalette">X</i>
+      <ul>
+        <li v-for="button in buttons" key="button.id">
+          <button :style="button.color" @click="setColor(button.color)"></button>
+        </li>
+      </ul>
+    </div>
 
   </div>
 </template>
 
 
 <script>
-  import ItemList from './components/ItemList.vue';
+import ItemList from "./components/ItemList.vue";
 
 export default {
   components: {
-    ItemList
+    ItemList,
   },
 
   data() {
     return {
       listStorage: window.localStorage,
-      list: JSON.parse(localStorage.getItem('pendingListStore')) || [],
-      doneList: JSON.parse(localStorage.getItem('doneListStore')) || [],
+      list: JSON.parse(localStorage.getItem("pendingListStore")) || [],
+      doneList: JSON.parse(localStorage.getItem("doneListStore")) || [],
       toDoItem: "",
+      buttons: [
+        {
+          id: 1,
+          color: "background-color: red",
+        },
+        {
+          id: 2,
+          color: "background-color: blue",
+        },
+        {
+          id: 3,
+          color: "background-color: yellow",
+        },
+        {
+          id: 4,
+          color: "background-color: green",
+        },
+        {
+          id: 5,
+          color: "background-color: orange",
+        }
+      ],
+
+      isVisible: false,
+
     };
   },
 
   methods: {
-    addToDo () {
+    addToDo() {
       if (this.toDoItem != "") {
-        this.list.push(this.toDoItem)
-        this.toDoItem = ""
-        this.listStorage.setItem("pendingListStore", JSON.stringify(this.list))
+        this.list.push(this.toDoItem);
+        this.toDoItem = "";
+        this.listStorage.setItem("pendingListStore", JSON.stringify(this.list));
       }
     },
 
-    markItem (item, category) {
+    markItem(item, category) {
       if (category == 1) {
-        this.list = this.list.filter((itemList) => itemList !== item)
-        this.doneList.push(item)
-        this.listStorage.setItem("pendingListStore", JSON.stringify(this.list))
-        this.listStorage.setItem("doneListStore", JSON.stringify(this.doneList))
-      } else if (category == 2) { 
-        this.doneList = this.doneList.filter((itemList) => itemList !== item)
-        this.list.push(item)
-        this.listStorage.setItem("pendingListStore", JSON.stringify(this.list))
-        this.listStorage.setItem("doneListStore", JSON.stringify(this.doneList))
-      }
-    },
-
-    cleanToDoList () {
-      this.listStorage.clear()
-      this.list = []
-      this.doneList = []
-      this.toDoItem = ""
-    },
-
-    deleteItem (item, category) {
-      if(category == 1) {
-        this.list = this.list.filter((itemList) => itemList !== item)
-        this.listStorage.setItem("pendingListStore", JSON.stringify(this.list))
+        this.list = this.list.filter((itemList) => itemList !== item);
+        this.doneList.push(item);
+        this.listStorage.setItem("pendingListStore", JSON.stringify(this.list));
+        this.listStorage.setItem(
+          "doneListStore",
+          JSON.stringify(this.doneList)
+        );
       } else if (category == 2) {
-        this.doneList = this.doneList.filter((itemList) => itemList !== item)
-        this.listStorage.setItem("doneListStore", JSON.stringify(this.doneList))
+        this.doneList = this.doneList.filter((itemList) => itemList !== item);
+        this.list.push(item);
+        this.listStorage.setItem("pendingListStore", JSON.stringify(this.list));
+        this.listStorage.setItem(
+          "doneListStore",
+          JSON.stringify(this.doneList)
+        );
       }
+    },
+
+    cleanToDoList() {
+      this.listStorage.clear();
+      this.list = [];
+      this.doneList = [];
+      this.toDoItem = "";
+    },
+
+    deleteItem(item, category) {
+      if (category == 1) {
+        this.list = this.list.filter((itemList) => itemList !== item);
+        this.listStorage.setItem("pendingListStore", JSON.stringify(this.list));
+      } else if (category == 2) {
+        this.doneList = this.doneList.filter((itemList) => itemList !== item);
+        this.listStorage.setItem(
+          "doneListStore",
+          JSON.stringify(this.doneList)
+        );
+      }
+    },
+
+    openColorPalette() {
+      this.isVisible = true
+    },
+
+    closeColorPalette() {
+      this.isVisible = false
+    },
+
+    setColor (color) {
+      console.log("Seteando: ", color)
     }
-  }
-}
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 
-  main {
-    border-radius: 1rem;
+main {
+  min-width: 350px;
+  min-height: 600px;
+  background-color: white;
+  padding: 1rem;
+  border-radius: 1rem;
+}
+
+#body {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  color: black;
+  
+  h1 {
+    margin: 1rem;
+    background-color: rgb(230, 230, 230);
   }
 
-  #body {
-    display: flex;
-    justify-content: center;
-    text-align: center;
-    color: black;
+  h2 {
+    font-size: large;
+    text-align: left;
+    text-decoration-line: underline;
+  }
 
-    h1 {
-      margin: 1rem;
+  input {
+    width: 250px;
+    padding: 0.5rem;
+    margin: 1rem 0;
+  }
+
+  button {
+    padding: 0.5rem;
+  }
+
+  #list {
+    span {
+      display: flex;
+
+      img {
+        margin-right: 0.5rem;
+      }
     }
 
-    h2 {
-      font-size:large;
-      text-align: left;
-      text-decoration-line: underline;
+    #pendingList {
+      min-height: 250px;
     }
 
-    input {
-      width: 250px;
-      padding: 0.5rem;
-      margin: 1rem 0;
-    }
+    #doneList {
+      min-height: 200px;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
 
-    button {
-      padding: 0.5rem;
-    }
-
-    #list {
-      /* display: flex;
-      flex-direction: column; */
-
-      #pendingList {
-        min-height: 250px;
+      span {
+        text-decoration: line-through;
       }
 
-      #doneList {
-        min-height: 200px;
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-        
-        span {
-          text-decoration: line-through;
-        }
-
-        .tip {
-          color: gray;
-          font-size: small;
-        }
+      .tip {
+        color: gray;
+        font-size: small;
       }
+    }
+    .listItem {
+      display: flex;
+      flex-direction: column;
+    }
 
-      ul {
-        padding: 1rem;
-        text-align: start;
+    .itemIcons { 
+      display: flex;
 
-        li {
+      .iconColors {
+        display: none;
+      }
+    }
+
+    ul {
+      padding: 1rem 0.5rem;
+      text-align: start;
+
+      li {
         color: black;
         display: flex;
         justify-content: space-between;
         margin: 0.2rem 0;
-        overflow:auto;
+        overflow: auto;
 
-          span {
-            font-weight: 600;
-          }
-        
-          i{  
-            color: red;
-          }
-
-          i:hover {
-            cursor: pointer;
-          }
+        span {
+          font-weight: 600;
         }
 
-        li:hover {
-          background-color: rgb(175, 175, 175, 0.2);
-          cursor: pointer
+        i {
+          color: red;
+        }
+
+        i:hover {
+          cursor: pointer;
+        }
+      }
+
+      li:hover {
+        background-color: rgb(175, 175, 175, 0.2);
+        cursor: pointer;
+
+        .iconColors {
+        display: flex;
         }
       }
     }
+
+  }
+}
+.colorPalette {
+  display: flex;
+  background-color: white;
+  flex-direction: column;
+  align-items: flex-end;
+  padding: 0.5rem;
+  border-radius: 3px;
+
+  ul {
+    list-style: none;
+    padding: 0;
   }
 
-  main{
-    min-width: 350px;
-    min-height: 600px;
-    background-color: white;
-    padding: 1rem;
+  button{
+    width: 100px;
+    display: block;
+    border-radius: 5px;
   }
+
+  i:hover {
+    cursor: pointer;
+  }
+}
+
+
 </style>
